e7a17884b1a799f3a3f379c4c303295bad616ddf
Matthew Garza <mattg614@gmail.com>
2022-02-20 10:33:51 -0500

    Add pseudocode for computer play

--- javascript.js
+++ javascript.js
@@ -1,1 +1,22 @@
-console.log("Hello");
+//Goal is to creat a Rock Paper Scissors Game for the Console
+//Rock Paper Scissors needs to take the input from the user
+//Compare the input to what the computer has selected
+//Determine who won or if a tie took place
+//All of this will be wrapped in a play game function that continues for 5 rounds
+
+//inputs: players choice of Rock Paper or Scissors
+
+//output: Result of players choice against computers choice (tie,win,loss)
+//output: total score for game that increments and notifies after computer or person gets to five
+
+
+//computer play
+//take players input for Rock Paper Scissors
+
+   //perform check that player input is valid
+
+//select Rock Paper Scissor for computer
+
+//compare player and computer choices and output
+
+
